b9aaa79755090e5bc95d388b84819dc80e7b685f
B1ZON-c0de <roman.andreicko@icloud.com>
2026-02-24 03:16:00 +0700

    rename server.js

--- api/server.js
+++
@@ -1,9 +0,0 @@
-const server = require('json-server')
-const server = server.create()
-const router = server.router('db.json')
-const middlewares = server.defaults()
-
-server.use(middlewares)
-server.use('/api', router)
-
-module.exports = server
